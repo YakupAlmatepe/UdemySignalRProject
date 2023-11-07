3e6549c00f500e57d86bfac4a39945b983681b11
Yakup Almatepe <almatepe.yakup@gmail.com>
2023-11-07 20:00:49 +0300

    DATA AccessLayaer EntityFramework sinifları

---
+++ SignalR.EntityLayer/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.EntityLayer.Entities
+{
+	public class Category
+	{
+        [Key] 
+        public int CatregoryID { get; set; }
+        public string CatregoryName { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ SignalR.EntityLayer/Entities/OpenHour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.EntityLayer.Entities
+{
+	public class OpenHour
+	{
+		[Key]
+		public int OpenHoursID { get; set; }
+        public string Description { get; set; }
+    }
+}
